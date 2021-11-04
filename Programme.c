db5910e848fc3d632096aebe397e33a659e05f00
Lilian BAUDRY <lilian.baudry21@gmail.com>
2021-11-05 00:55:28 +0100

    Affichage CSV - Version 1

--- Programme.c
+++ Programme.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+
+typedef struct donnees{
+	char nom[20], prenom[20];
+	char ville[20], codepostal[5];
+	char tel[10], email[40];
+	char profession[20];
+}personne;
+
 
 int main()
 {
-    int nb;
-    int i;
-    printf("Rentrez un nombre : ");
-    scanf("%d",&nb);
-    for (i = 0; i < nb; i++)
-        printf("Salut mec!\n");
-    printf("Il a dit %d fois, bonjour !", nb);
-    return 0; 
+	FILE *annuaire = fopen("Annuaire.csv","r");
+	char ligne[135];
+	int i;
+	int nbrow=1;
+
+	while(fgets(ligne,sizeof(ligne),annuaire) != NULL && nbrow<=10){
+		printf("\n\nPersonne numero %d :\n-	", nbrow);
+		for(i=0;ligne[i]!='\n';i++){
+			if (ligne[i]==',')
+				printf("\n-	");
+			else
+				printf("%c", ligne[i]);
+		}
+		nbrow++;
+	}
 }
